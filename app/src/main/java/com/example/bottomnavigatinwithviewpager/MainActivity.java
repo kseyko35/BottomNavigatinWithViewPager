c6a6e0dfd0d0630f494e2e04b85f9d52bc836d2f
Seyfi Ercan <s.ercan@amatis.nl>
2019-10-07 17:05:22 +0300

    Initial commit

---
+++ app/src/main/java/com/example/bottomnavigatinwithviewpager/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.bottomnavigatinwithviewpager;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.viewPager)
+    ViewPager mViewPager;
+    @BindView(R.id.bottomNavigationView)
+    BottomNavigationView mBottomNavigationView;
+    private MenuItem prevMenuItem;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.action_item1:
+                        mViewPager.setCurrentItem(0);
+                        break;
+                    case R.id.action_item2:
+                        mViewPager.setCurrentItem(1);
+                        break;
+                    case R.id.action_item3:
+                        mViewPager.setCurrentItem(2);
+                        break;
+                }
+                return false;
+            }
+        });
+
+        mViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (prevMenuItem != null) {
+                    prevMenuItem.setChecked(false);
+                }
+                else
+                {
+                    mBottomNavigationView.getMenu().getItem(0).setChecked(false);
+                }
+
+                mBottomNavigationView.getMenu().getItem(position).setChecked(true);
+                prevMenuItem = mBottomNavigationView.getMenu().getItem(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+
+        setupViewPager(mViewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager)
+    {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFragment(TodayFragment.newInstance());
+        adapter.addFragment(TomorrowFragment.newInstance());
+        adapter.addFragment(NextDayFragment.newInstance());
+        viewPager.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/com/example/bottomnavigatinwithviewpager/TodayFragment.java
@@ -0,0 +1,32 @@
+package com.example.bottomnavigatinwithviewpager;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class TodayFragment extends Fragment {
+
+
+    public static TodayFragment newInstance() {
+        TodayFragment fragment = new TodayFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        return inflater.inflate(R.layout.fragment_today, container, false);
+    }
+
+}
